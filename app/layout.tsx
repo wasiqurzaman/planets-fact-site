1a048d31aef9f46d75272041ba17dcf92deefb5e
Wasiqur Zaman <wasiqurzaman@gmail.com>
2024-12-24 21:33:12 +0530

    Configured custom colors and fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,17 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Antonio, League_Spartan } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const antonio = Antonio({
   subsets: ["latin"],
+  display: "swap",
+  variable: "--font-antonio",
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const spartan = League_Spartan({
   subsets: ["latin"],
+  display: "swap",
+  variable: "--font-spartan",
 });
 
 export const metadata: Metadata = {
@@ -25,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${antonio.variable} ${spartan.variable} antialiased bg-stars bg-black-100 bg-cover bg-no-repeat`}
       >
         {children}
       </body>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,26 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        // background: "var(--background)",
+        // foreground: "var(--foreground)",
+        "black-100": "var(--color-black)",
+        "light-grey": "var(--color-light-gray)",
+        "dark-gray": "var(--color-dark-gray)",
+        mercury: "var(--color-mercury)",
+        venus: "var(--color-venus)",
+        earth: "var(--color-earth)",
+        mars: "var(--color-mars)",
+        jupiter: "var(--color-jupiter)",
+        saturn: "var(--color-saturn)",
+        uranus: "var(--color-uranus)",
+        neptune: "var(--color-neptune)",
+      },
+      fontFamily: {
+        antonio: ["var(--font-antonio)"],
+        spartan: ["var(--font-spartan)"],
+      },
+      backgroundImage: {
+        stars: "url('/images/background-stars.svg')",
       },
     },
   },
