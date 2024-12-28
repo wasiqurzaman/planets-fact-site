03b991c2d4d7ed72acc8452489eeab77dfeecbc8
Wasiqur Zaman <wasiqurzaman@gmail.com>
2024-12-28 20:20:12 +0530

    add the link to the home

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import NavLinks from "./NavLinks";
 import { Planet } from "../layout";
 import { useState } from "react";
+import Link from "next/link";
 
 interface Props {
   data: Planet[];
@@ -16,9 +17,12 @@ export default function Navbar({ data }: Props) {
   return (
     <nav className="max-w-[1440px] mx-auto flex flex-col items-start tablet:flex-col tablet:items-center laptop-sm:flex-row  md:items-center md:justify-between text-white laptop-sm:py-3 pt-4 px-10 large-desktop:px-0">
       <div className="flex items-center justify-between self-stretch tablet:self-center pb-4 tablet:pb-0">
-        <span className="font-antonio text-3xl uppercase tracking-[-1.05px]">
+        <Link
+          href="/planets"
+          className="font-antonio text-3xl uppercase tracking-[-1.05px]"
+        >
           The Planets
-        </span>
+        </Link>
         <Image
           src={hamburger}
           alt="hamburger menu icon for mobile navigation"
